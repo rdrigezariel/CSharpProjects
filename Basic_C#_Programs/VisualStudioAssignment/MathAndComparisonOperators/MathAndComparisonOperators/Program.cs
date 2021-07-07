b328560a25e272bd264775acdb6a7de3ce5487e9
Ariel <rdrigezariel@gmail.com>
2021-07-07 13:09:29 -0400

    Update MathAndComparisonOperators Assignment

---
+++ Basic_C#_Programs/VisualStudioAssignment/MathAndComparisonOperators/MathAndComparisonOperators/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MathAndComparisonOperators
+{
+    class Program
+    {
+        static void Main()
+        {
+            // First exercise (addition)
+            Console.WriteLine("Addition");
+            int total = 5 + 10;
+            int otherTotal = 12 + 22;
+            int combined = total + otherTotal;
+            Console.WriteLine(combined);
+
+            // Second exercise (subtraction)
+            Console.WriteLine("\nSubtraction");
+            int difference = 10 - 5;
+            Console.WriteLine(difference.ToString());
+
+            // Third exercise (multiplication)
+            Console.WriteLine("\nMultiplication");
+            int product = 10 * 5;
+            Console.WriteLine(product);
+
+            // Fourth exercise (division)
+            Console.WriteLine("\nDivision");
+            double quotient = 100.0 / 17.0;
+            Console.WriteLine(quotient);
+
+            // Fifth exercise (remainder)
+            Console.WriteLine("\nRemainder");
+            int remainder = 10 % 3;
+            Console.WriteLine(remainder);
+            
+
+        }
+    }
+}
