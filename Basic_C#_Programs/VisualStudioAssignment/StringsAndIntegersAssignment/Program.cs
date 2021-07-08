f820fdd894023d319e57bb0cf85cdacfb0773cca
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-07-07 23:15:49 -0400

    Upload StringsAndIntegersAssignment

---
+++ Basic_C#_Programs/VisualStudioAssignment/StringsAndIntegersAssignment/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+static class Program
+{
+    static void Main()
+    {
+        // Create a list of integers
+        List<int> myList = new List<int>(new int[] { 1, 2, 3, 4, 5 });
+
+        try
+        {
+            // Ask user for a number to divide each number in myList.
+            Console.WriteLine("Please enter a number to divide each number in my list by: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            foreach (int number in myList)
+            {
+                int result = number / input;
+                Console.WriteLine("Dividing " + number + " by " + input + ". The result is: " + result + ".");
+            }
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Error, cannot divide by zero.");
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Error, that is not a number!");
+        }
+    }
+}
