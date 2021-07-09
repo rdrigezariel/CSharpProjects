9bdde52f5a739e1239d9e783a141ff5f71ddeb53
Ariel <rdrigezariel@gmail.com>
2021-07-08 20:15:52 -0400

    Push Assignment

---
+++ Basic_C#_Programs/VisualStudioAssignment/TwentryOne-ClassesAndObjects/TwentryOne-ClassesAndObjects/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace TwentryOne_ClassesAndObjects
+{
+    static class Program
+    {
+        static void Main()
+        {
+            Deck deck = new Deck();
+
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.ReadLine();
+        }
+    }
+}
