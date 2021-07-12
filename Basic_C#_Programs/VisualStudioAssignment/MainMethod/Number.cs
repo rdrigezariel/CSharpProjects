cdfd64ca9c1594f7c9ea14e763b3d74343887ecf
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-07-12 19:47:27 -0400

    Create MainMethod Project

---
+++ Basic_C#_Programs/VisualStudioAssignment/MainMethod/Number.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Number
+    {
+        // MethodOne will return a input of type int plus 2 as an integer.
+        public int MethodOne(int input) => input + 2;
+        // MethodTwo will return a input of type decimal minus 2 as an integer.
+        public int MethodTwo(double input) => (int)(input - 2);
+        // MethodThree will convert the string type input as an integer then return input plus 1 as an integer.
+        public int MethodThree(string input) => Convert.ToInt32(input) + 2;
+    }
+}
---
+++ Basic_C#_Programs/VisualStudioAssignment/MainMethod/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize objOne
+            Number objOne = new Number();
+            // Call MethodOne from objOne passing in a int type parameter of 1
+            Console.WriteLine("MethodOne Results: {0}", objOne.MethodOne(1));
+
+            // Initialize objTwo
+            Number objTwo = new Number();
+            // Call MethodTwo from objTwo passing in a double type parameter of 3.5
+            Console.WriteLine("MethodTwo Results: {0}", objTwo.MethodTwo(3.5));
+
+            // Initialize objThree
+            Number objThree = new Number();
+            // Call MethodThree from objThree passing in a string type parameter of 2
+            // Add a try catch to make sure the parameter can be converted to a integer.
+            try
+            {
+                Console.WriteLine("MethodThree Results: {0}", objThree.MethodThree("2"));
+            }
+            catch (FormatException)
+            {
+
+                Console.WriteLine("Cannot product results for MethodThree, cannot convert string to int");
+            }
+            Console.ReadLine();
+        }
+    }
+}
