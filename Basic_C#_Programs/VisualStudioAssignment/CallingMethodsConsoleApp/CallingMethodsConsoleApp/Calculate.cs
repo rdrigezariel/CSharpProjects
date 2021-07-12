77bda7f340ea2f25ea5e03711597eb3adb3e96ae
Ariel <rdrigezariel@gmail.com>
2021-07-12 17:29:59 -0400

    Update CallingMethods Project

--- Basic_C#_Programs/VisualStudioAssignment/CallingMethodsConsoleApp/CallingMethodsConsoleApp/Calculate.cs
+++ Basic_C#_Programs/VisualStudioAssignment/CallingMethodsConsoleApp/CallingMethodsConsoleApp/Calculate.cs
@@ -9,5 +9,23 @@ namespace CallingMethodsConsoleApp
     class Calculate
     {
         public int Input { get; set; }
+
+        public int AddTwo(int input)
+        {
+            // Adding two to users input
+            return input + 2;
+        }
+
+        public int MinusTwo(int input)
+        {
+            // Subtracting two from users input
+            return input - 2;
+        }
+
+        public int TimesTwo(int input)
+        {
+            // Multiplying users input by 2
+            return input * 2;
+        }
     }
 }
--- Basic_C#_Programs/VisualStudioAssignment/CallingMethodsConsoleApp/CallingMethodsConsoleApp/Program.cs
+++ Basic_C#_Programs/VisualStudioAssignment/CallingMethodsConsoleApp/CallingMethodsConsoleApp/Program.cs
@@ -20,28 +20,10 @@ namespace CallingMethodsConsoleApp
             input.Input = Convert.ToInt32(Console.ReadLine());
 
             // Display results of each method to the user
-            Console.WriteLine("Calling the \"AddTwo\" method returns: " + AddTwo(input.Input));
-            Console.WriteLine("Calling the \"MinusTwo\" method returns: " + MinusTwo(input.Input));
-            Console.WriteLine("Calling the \"TimesTwo\" method returns: " + TimesTwo(input.Input));
+            Console.WriteLine("Calling the \"AddTwo\" method returns: " + input.AddTwo(input.Input));
+            Console.WriteLine("Calling the \"MinusTwo\" method returns: " + input.MinusTwo(input.Input));
+            Console.WriteLine("Calling the \"TimesTwo\" method returns: " + input.TimesTwo(input.Input));
             Console.ReadLine();
-        }
-
-        public static int AddTwo(int input)
-        {
-            // Adding two to users input
-            return input + 2;
-        }
-
-        public static int MinusTwo(int input)
-        {
-            // Subtracting two from users input
-            return input - 2;
-        }
-
-        public static int TimesTwo(int input)
-        {
-            // Multiplying users input by 2
-            return input * 2;
         }
     }
 }
