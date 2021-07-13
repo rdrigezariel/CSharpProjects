e6a098347c7cce384c08ef521fbbe2a5dde10669
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-07-12 21:04:53 -0400

    Create ClassSubmission Project

---
+++ Basic_C#_Programs/VisualStudioAssignment/ClassSubmission/ClassOne.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class ClassOne
+    {
+        // Created a void methid that outputs an int.
+        // The method divides the input by 2.
+        // The method also contains a overload.
+        public void MethodOne(int input, int overload = 1)
+        {
+            Console.WriteLine("{0} divided by 2 equals: {1}", input, (input / 2));
+            Console.WriteLine("Overload value: {0}", overload);
+        }
+
+        // Created a method with output parameters
+        public void MethodOutputParams(out int input)
+        {
+            input = 2;
+        }
+    }
+
+    // Declare static class ClassTwo
+    static class ClassTwo
+    {
+    }
+}
---
+++ Basic_C#_Programs/VisualStudioAssignment/ClassSubmission/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize obj
+            ClassOne obj = new ClassOne();
+            // Prompt the user to enter a number
+            Console.WriteLine("Enter a number: ");
+            try
+            {
+                // Convert the users string input into a int
+                int input = Convert.ToInt32(Console.ReadLine());
+                // Call MethodOne
+                obj.MethodOne(input);
+            }
+            catch (FormatException)
+            {
+                // This catch will look for format errors,
+                // incase the user does not enter a integer.
+                Console.WriteLine("Error, please enter a number.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
