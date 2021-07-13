d235f6798e94c315cd7532fcd7efa5e3b875fa69
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-07-12 20:09:09 -0400

    Create MethodClassSubmission Project

---
+++ Basic_C#_Programs/VisualStudioAssignment/MethodClassSubmission/Calculate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Calculate
+    {
+        public void OddMethod(int one, int two)
+        {
+            // Add one to 'one'
+            one++;
+            // Display two to the screen
+            Console.WriteLine("Second integer = {0}", two);
+        }
+    }
+}
---
+++ Basic_C#_Programs/VisualStudioAssignment/MethodClassSubmission/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassSubmission
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Initialize obj
+            Calculate obj = new Calculate();
+
+            // Calling the OddMethod, passing in 1 and 2
+            obj.OddMethod(1, 2);
+            // Calling the OddMethod, passing in 1 and 3 but specifying the params by name
+            obj.OddMethod(one: 1, two: 3);
+            Console.ReadLine();
+        }
+    }
+}
