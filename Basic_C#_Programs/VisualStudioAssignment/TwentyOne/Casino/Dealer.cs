0ee8851488d9e02aa8dcdc9e3ab0057bfd40ac10
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-07-15 10:24:46 -0400

    Update TwentyOne Project

---
+++ Basic_C#_Programs/VisualStudioAssignment/TwentyOne/Casino/Dealer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    internal class Dealer
+    {
+        public string Name { get; set; }
+        public Deck Deck { get; set; }
+        public int Balance { get; set; }
+
+        public void Deal(List<Card> Hand)
+        {
+            Hand.Add(Deck.Cards.First());
+            string card = string.Format(Deck.Cards.First().ToString() + "\n");
+            Console.WriteLine(card);
+            using (StreamWriter file = new StreamWriter(@"C:\Users\Ariel\Desktop\.log2.txt", true))
+            {
+                file.WriteLine(DateTime.Now);
+                file.WriteLine(card);
+            }
+            Deck.Cards.RemoveAt(0);
+        }
+    }
+}
---
+++ Basic_C#_Programs/VisualStudioAssignment/TwentyOne/TwentyOne/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Casino;
+using Casino.TwentyOne;
+
+namespace TwentyOne
+{
+    static class Program
+    {
+        static void Main()
+        {
+            //string text = "Here is some text.";
+            //File.WriteAllText(@"C:\Users\Ariel\Desktop\log.txt", text);
+
+            DateTime dateTime = new DateTime(1995, 5, 23, 8, 32, 45);
+            DateTime yearOfGraduation = new DateTime(2013, 6, 1, 16, 34, 22);
+            TimeSpan ageAtGraduation = yearOfGraduation - dateTime;
+            Console.WriteLine("Welcome to the Grand Hotel and Casino. Let's start by telling me your name.");
+            string playerName = Console.ReadLine();
+            Console.WriteLine("How much money did you bring today?");
+            int bank = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
+            string answer = Console.ReadLine().ToLower();
+
+            if (answer == "yes" || answer == "yeah" || answer == "y" || answer == "ya")
+            {
+                Player player = new Player(playerName, bank);
+                Game game = new TwentyOneGame();
+                game += player;
+                Console.WriteLine("Current players in game: {0}", game.Players.Count);
+                player.isActivelyPlaying = true;
+                while (player.isActivelyPlaying && player.Balance > 0)
+                {
+                    game.Play();
+                }
+                game -= player;
+                Console.WriteLine("Thank you for playing!");
+                Console.WriteLine("Current players in game: {0}", game.Players.Count);
+            }
+            Console.WriteLine("Feel free to look around the casino. Bye for now");
+            Console.ReadLine();
+        }
+    }
+}
