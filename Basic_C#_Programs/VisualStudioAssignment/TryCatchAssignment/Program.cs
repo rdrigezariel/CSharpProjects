4dc76cf41af970cfca844ea27dc8681ab27ba45c
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2021-07-16 23:38:33 -0400

    Create TryCatchAssignment Project

---
+++ Basic_C#_Programs/VisualStudioAssignment/TryCatchAssignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatchAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Get current year in int type
+            int currentYear = Convert.ToInt32(DateTime.Today.ToString("yyyy"));
+            Console.WriteLine("Enter your age");
+
+            try
+            {
+                // Get users born year
+                var yearBorn = Convert.ToInt32(DateTime.Today.ToString(Console.ReadLine()));
+                // Determine users age
+                int usersAge = currentYear - yearBorn;
+
+                Console.WriteLine("You were born in the year {0}.", usersAge);
+            }
+            catch (Exception)
+            {
+                // User didn't enter a pure int type
+                Console.WriteLine("Please contact an Admin Administrator for assistance.");
+            }
+            // Pause program
+            Console.ReadLine();
+        }
+    }
+}
--- Basic_C#_Programs/VisualStudioAssignment/TwentyOne/TwentyOne/Program.cs
+++ Basic_C#_Programs/VisualStudioAssignment/TwentyOne/TwentyOne/Program.cs
@@ -16,8 +16,19 @@ namespace TwentyOne
 
             Console.WriteLine("Welcome to the {0}. Let's start by telling me your name.", casinoName);
             string playerName = Console.ReadLine();
-            Console.WriteLine("How much money did you bring today?");
-            int bank = Convert.ToInt32(Console.ReadLine());
+
+            bool validAnswer = false;
+            int bank = 0;
+            while (!validAnswer)
+            {
+                Console.WriteLine("And how much money did you bring today?");
+                validAnswer = int.TryParse(Console.ReadLine(), out bank);
+                if (!validAnswer)
+                {
+                    Console.WriteLine("Please enter digits only, no decimals.");
+                }
+            }
+
             Console.WriteLine("Hello, {0}. Would you like to join a game of 21 right now?", playerName);
             string answer = Console.ReadLine().ToLower();
 
@@ -35,7 +46,23 @@ namespace TwentyOne
                 player.isActivelyPlaying = true;
                 while (player.isActivelyPlaying && player.Balance > 0)
                 {
-                    game.Play();
+                    try
+                    {
+                        game.Play();
+                    }
+                    catch (FraudException)
+                    {
+                        Console.WriteLine("Security! Kick this person out.");
+                        Console.ReadLine();
+                        return;
+                    }
+                    catch (Exception)
+                    {
+
+                        Console.WriteLine("An error occured. Please contact your System Administratior.");
+                        Console.ReadLine();
+                        return;
+                    }
                 }
                 game -= player;
                 Console.WriteLine("Thank you for playing!");
