4b80891aa7b110a19d673982efca4f72b2acda75
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-20 19:22:49 -0400

    initial commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,9 @@
+﻿
+// This is how you use the Console.WriteLine method
+// Also note that you can use the "cw" snippet to create
+// the Console.WriteLine method signature.
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine("How are you?");
+
+Console.ReadLine();
---
+++ HelloWorldInDotNET5/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldInDotNET5 
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello, world from .NET 5");
+        }
+    }
+}
