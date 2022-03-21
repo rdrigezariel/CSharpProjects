7a363b703bd271565d0d075ee727278028697c41
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-21 01:33:21 -0400

    add console application GreetingApp

---
+++ Projects 2022/MySecondProject/GreetingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+ Program steps:
+ - Welcome User to App
+ - Ask for First Name
+ - Greet user by name
+ */
+
+// Welcome User to App
+Console.WriteLine("Welcome to the Greeting Application!");
+Console.WriteLine("This application was built by Ariel Rodriguez");
+Console.WriteLine("---------------------------------------------\n");
+
+// Ask for First Name
+Console.Write("What is your first name: ");
+string firstName;
+firstName = Console.ReadLine();
+
+// Greet user by name
+Console.WriteLine($"Hello, {firstName}!");
+Console.WriteLine("Thank you for using my application.");
+Console.ReadLine();
