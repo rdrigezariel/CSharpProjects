08a46c60de1dad3c86483a154f83ca8298059741
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-21 03:10:36 -0400

    create DateTimeVariables project

---
+++ Projects 2022/DateTimeVariablesApp/DateTimeVariables/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+DateTime today = DateTime.Now;
+TimeOnly timeNow = TimeOnly.FromDateTime(DateTime.Now);
+DateOnly dateToday = DateOnly.FromDateTime(DateTime.Now);
+
+DateTime birthday = DateTime.Parse("06/30/1992");
+DateTime birthdayTwo = DateTime.ParseExact("06/30/1992", "d", CultureInfo.InvariantCulture);
+
+Console.WriteLine(today);
+Console.WriteLine(timeNow);
+Console.WriteLine(dateToday);
+Console.WriteLine(birthday.ToString(CultureInfo.InvariantCulture));
+Console.WriteLine(birthdayTwo);
