8a208435647ed4b1586bf31b94f12244c2c02039
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-21 01:59:00 -0400

    add StringVarApp

---
+++ Projects 2022/StringVariableApp/StringVariable/Program.cs
@@ -0,0 +1,11 @@
+﻿
+string firstName = string.Empty;
+string lastName = string.Empty;
+string filePath = string.Empty;
+
+firstName = "Ariel";
+lastName = "Rodriguez";
+filePath = @"C:\Temp\Demo";
+
+Console.WriteLine($"Hello {firstName} {lastName}");
+Console.WriteLine(filePath);
