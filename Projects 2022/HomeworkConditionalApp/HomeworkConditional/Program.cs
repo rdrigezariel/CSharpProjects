f484bbdbe6147d712c3f37baf03d728cccd148ca
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-22 00:42:14 -0400

    Create HomeworkConditionApp

---
+++ Projects 2022/HomeworkConditionalApp/HomeworkConditional/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+Console.Write("Please enter your first name: ");
+string firstName = Console.ReadLine();
+
+Console.WriteLine("Call from IF statement");
+if (firstName.ToLower() == "tim" || firstName.ToLower() == "timothy") Console.WriteLine("Hello, Professor!");
+else Console.WriteLine("Hello, Student!");
+
+Console.WriteLine("Call from Switch statement");
+switch (firstName.ToLower())
+{
+    case "tim" or "timothy":
+        Console.WriteLine("Hello, Professor!");
+        break;
+    default:
+        Console.WriteLine("Hello, Student!");
+        break;
+}
