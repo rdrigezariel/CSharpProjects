9841c2dc83c92c05757200054e372672af2598c7
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-22 01:23:38 -0400

    Add MiniProjectStudentCheckApp

---
+++ Projects 2022/MiniProjectStudentCheckApp/MiniProjectStudentCheck/Program.cs
@@ -0,0 +1,19 @@
+﻿
+Console.Write("Please enter your first name: ");
+string firstName = Console.ReadLine();
+string formattedName = firstName;
+
+Console.Write("Please enter your age: ");
+if (!int.TryParse(Console.ReadLine(), out int age))
+{
+    Console.WriteLine("You did not provide a valid age, run program again");
+    return;
+}
+
+if (firstName.ToLower() == "bob" || firstName.ToLower() == "sue") 
+    formattedName = $"Professor {firstName}";
+
+if (age < 21) Console.WriteLine($"I recommend you wait {21 - age} year(s), {formattedName}");
+else Console.WriteLine($"Welcome to class, {formattedName}");
+
+
