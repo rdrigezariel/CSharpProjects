cb6a9fc04affc3a2bbd63b89f562bb7a5b3f0787
Ariel Rodriguez <69484552+rdrigezariel@users.noreply.github.com>
2022-03-22 00:34:58 -0400

    add advancedifapp

---
+++ Projects 2022/AdvancedIfApp/AdvancedIf/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+Console.Write("What is your first name: ");
+string firstName = Console.ReadLine();
+
+Console.Write("What is your last name: ");
+string lastName = Console.ReadLine();
+
+if (firstName.ToLower() == "tim" && lastName.ToLower() == "corey") Console.WriteLine("Hello, Mr. Corey");
--- Projects 2022/DateTimeVariablesApp/DateTimeVariables/Program.cs
+++ Projects 2022/DateTimeVariablesApp/DateTimeVariables/Program.cs
@@ -1,14 +1,18 @@
-﻿using System.Globalization;
+﻿
+Console.Write("Please enter your age: ");
+bool isValid = int.TryParse(Console.ReadLine(), out int usersAge);
 
-DateTime today = DateTime.Now;
-TimeOnly timeNow = TimeOnly.FromDateTime(DateTime.Now);
-DateOnly dateToday = DateOnly.FromDateTime(DateTime.Now);
+if (isValid)
+{
+    int futureAge = usersAge + 25;
+    int pastAge = usersAge - 25;
+    
+    Console.WriteLine($"In 25 years, you will be: {futureAge}");
+    Console.WriteLine($"25 years ago, you were: {pastAge}");
+}
+else
+{
+    Console.WriteLine("Invalid age, please try again!");
+}
 
-DateTime birthday = DateTime.Parse("06/30/1992");
-DateTime birthdayTwo = DateTime.ParseExact("06/30/1992", "d", CultureInfo.InvariantCulture);
 
-Console.WriteLine(today);
-Console.WriteLine(timeNow);
-Console.WriteLine(dateToday);
-Console.WriteLine(birthday.ToString(CultureInfo.InvariantCulture));
-Console.WriteLine(birthdayTwo);
